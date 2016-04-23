de1b3c9b7afa687b076528266a512cf5df69312f
roxel <piotr@roksela.pl>
2016-04-23 22:23:52 +0200

    basic database setup

---
+++ app/planner/models.py
@@ -0,0 +1,23 @@
+from app.database import Base
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, Date
+from datetime import datetime
+
+class Task(Base):
+    __tablename__ = 'tasks'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(200))
+    date = Column(Date)
+    done = Column(Boolean)
+    priority = Column(Integer)
+    date_created = Column(DateTime)
+
+    def __init__(self, name, date, priority=1):
+        self.name = name
+        self.date = date
+        self.done = False
+        self.priority = priority
+        self.date_created = datetime.utcnow()
+
+    def __repr__(self):
+        return '<Task {}: {}>'.format(self.id, self.name)
+
--- basic_config.py
+++ basic_config.py
@@ -1,4 +1,6 @@
+import os
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))+os.path.sep
+DATABASE_ADDRESS = 'sqlite:///' + PROJECT_DIR + "sqlite.db"
 SECRET_KEY = "default_secret_key"
-
 DEBUG = False
