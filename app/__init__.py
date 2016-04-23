28899ccf40006ade3a021917156743bc78a0a009
roxel <piotr@roksela.pl>
2016-04-23 20:47:45 +0200

    basic routing

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask, render_template, redirect
 
 from .planner import planner
 
@@ -17,13 +17,21 @@ def register_blueprints(app):
 def configure_general(app):
     @app.route("/")
     def show_index():
-        return "this is index page", 200
+        return redirect("planner")
+
+
+def configure_errors(app):
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template("layout/page_not_found.html"), 404
+
 
 
 def create_app():
     app = Flask(__name__)
     register_blueprints(app)
     configure_general(app)
+    configure_errors(app)
     app.config.from_object('config')
     return app
 
--- app/planner/views.py
+++ app/planner/views.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 planner = Blueprint('planner', __name__, url_prefix='/planner')
 
 @planner.route('/')
 def show_index():
-    return "this is planner page", 200
+    return render_template("planner/index.html")
