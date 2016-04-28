682bb4d3e92dc4618c3150d9cec54c30f87c44fe
roxel <piotr@roksela.pl>
2016-04-28 11:51:12 +0200

    forms macros and views refactoring

--- app/helpers.py
+++ app/helpers.py
@@ -1,3 +1,4 @@
+from flask import request, url_for
 from werkzeug.routing import BaseConverter
 from datetime import datetime
 
@@ -10,3 +11,7 @@ class RegexConverter(BaseConverter):
 
 def get_date_from_date_string(date_string):
     return datetime.strptime(date_string, '%Y-%m-%d').date()
+
+
+def redirect_url(default='planner.show_index'):
+    return request.args.get('next') or request.referrer or url_for(default)
